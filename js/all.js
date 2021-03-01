7487808416279801eed3e0a0cc364a40320999b0
jesulonimii <jesulonimii.will@gmail.com>
2021-03-01 19:49:28 +0100

    portfolio update - animate back to top button

--- js/all.js
+++ js/all.js
@@ -1,10 +1,16 @@
-// When the user scrolls down 20px from the top of the document, show the button
+
+// handling scroll to top button
+
 var btTopButton = document.querySelector(".back-to-top-btn")
 
+
+//check if user scrolls
 window.onscroll = function() {scrollFunction()};
 
+
+//show back to top button
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.documentElement.scrollTop > 30) {
     btTopButton.style.display = "block";
   } else {
     btTopButton.style.display = "none";
@@ -13,9 +19,15 @@ function scrollFunction() {
 
 function backToTop() {
 
-     document.body.scrollTop = 0; // For Safari
-     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
+     document.documentElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
 
 
 
 }
+
+
+// handling scroll to top button end ***
